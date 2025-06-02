f4e7033e9d21d1ab05e9ef23935e0bda0b864061
nileshmishra280 <nileshmishra2508@gmail.com>
2025-06-02 11:03:13 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db'); // MongoDB connection
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
